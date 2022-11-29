ed76f00f88bdb7fcf22cb00555039be549c86338
Hossein Mohammadi <hosseinm.developer@gmail.com>
2022-11-29 17:53:41 +0330

    add sync functions to index

--- js/index.ts
+++ js/index.ts
@@ -1,46 +1,109 @@
 import { Platform } from "react-native";
 import RNNetworkInfo from "./NativeNetworkInfo";
 
+export function getSyncSSID() {
+  return RNNetworkInfo?.getSyncSSID();
+}
+
+export function getSyncBSSID() {
+  return RNNetworkInfo?.getSyncBSSID();
+}
+
+export function getSyncBroadcast() {
+  return RNNetworkInfo?.getSyncBroadcast();
+}
+
+export function getSyncIPAddress() {
+  return RNNetworkInfo?.getSyncIPAddress();
+}
+
+export function getSyncIPV4Address() {
+  const wifiIP = RNNetworkInfo?.getSyncWIFIIPV4Address();
+  if (wifiIP && wifiIP !== "0.0.0.0") {
+    return wifiIP;
+  }
+
+  return RNNetworkInfo?.getSyncIPV4Address();
+}
+
+export function getSyncSubnet() {
+  return RNNetworkInfo?.getSyncSubnet();
+}
+
+export function getSyncGatewayIPAddress() {
+  return RNNetworkInfo?.getSyncGatewayIPAddress();
+}
+
+/**
+ * Android Only
+ */
+export function getSyncFrequency() {
+  if (Platform.OS !== "android") {
+    return null;
+  }
+}
+
+export async function getSSID() {
+  return await RNNetworkInfo?.getSSID();
+}
+
+export async function getBSSID() {
+  return await RNNetworkInfo?.getBSSID();
+}
+
+export async function getBroadcast() {
+  return await RNNetworkInfo?.getBroadcast();
+}
+
+export async function getIPAddress() {
+  return await RNNetworkInfo?.getIPAddress();
+}
+
+export async function getIPV4Address() {
+  const wifiIP = await RNNetworkInfo?.getWIFIIPV4Address();
+  if (wifiIP && wifiIP !== "0.0.0.0") {
+    return wifiIP;
+  }
+
+  return await RNNetworkInfo?.getIPV4Address();
+}
+
+export async function getGatewayIPAddress() {
+  return await RNNetworkInfo?.getGatewayIPAddress();
+}
+
+export async function getSubnet() {
+  return await RNNetworkInfo?.getSubnet();
+}
+
+/**
+ * Android Only
+ */
+export async function getFrequency() {
+  if (Platform.OS !== "android") {
+    return null;
+  }
+  return await RNNetworkInfo?.getFrequency();
+}
+
 const NetworkInfo = {
-  async getSSID() {
-    return await RNNetworkInfo?.getSSID();
-  },
-
-  async getBSSID() {
-    return await RNNetworkInfo?.getBSSID();
-  },
-
-  async getBroadcast() {
-    return await RNNetworkInfo?.getBroadcast();
-  },
-
-  async getIPAddress() {
-    return await RNNetworkInfo?.getIPAddress();
-  },
-
-  async getIPV4Address() {
-    const wifiIP = await RNNetworkInfo?.getWIFIIPV4Address();
-    if (wifiIP && wifiIP !== "0.0.0.0") {
-      return wifiIP;
-    }
-
-    return await RNNetworkInfo?.getIPV4Address();
-  },
-
-  async getGatewayIPAddress() {
-    return await RNNetworkInfo?.getGatewayIPAddress();
-  },
-
-  async getSubnet() {
-    return await RNNetworkInfo?.getSubnet();
-  },
-
-  async getFrequency() {
-    if (Platform.OS !== "android") {
-      return null;
-    }
-    return await RNNetworkInfo?.getFrequency();
-  },
+  getSyncSSID,
+  getSyncBSSID,
+  getSyncBroadcast,
+  getSyncIPAddress,
+  getSyncIPV4Address,
+  getSyncSubnet,
+  getSyncGatewayIPAddress,
+  getSyncFrequency,
+  getSSID,
+  getBSSID,
+  getBroadcast,
+  getIPAddress,
+  getIPV4Address,
+  getGatewayIPAddress,
+  getSubnet,
+  getFrequency,
 };
 
 export default NetworkInfo;
+export { NetworkInfo };
